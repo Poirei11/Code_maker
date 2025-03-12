cc9a6132ffcc55e6bfc3b285292b5d44bb70f112
Poirei11 <thokchompoirei4@gmail.com>
2025-03-12 14:15:35 +0530

    program1

---
+++ Comment/Yuu.java
@@ -0,0 +1,6 @@
+public class Yuu{
+    public static void main(String args[]){
+        int num1 = 2; // the value is 2
+        System.out.println(num1); // this lne is used to print the value
+    }
+}
