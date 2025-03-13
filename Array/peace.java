7cd63a011f725608f9849098b339b9e3b51bd592
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 12:10:37 +0530

    Peace

---
+++ Array/peace.java
@@ -0,0 +1,8 @@
+public class peace{
+    public static void main(String args[]){
+        int a[] = {11,23,45,67,44};
+        System.out.println(a[0]);
+        System.out.println(a[3]);    
+    }
+
+}
