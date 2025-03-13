7969007f7eda52f045418f9a2f63b3613649a162
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 14:33:07 +0530

    peace

---
+++ Leap_year/peace.java
@@ -0,0 +1,11 @@
+public class peace{
+    public static void main(String args[]){
+        int year = 2004;
+        if(year%4==0){
+            System.out.println("Leap year");        
+            }
+            else{
+                System.out.println("not a leap year");
+            }
+    }
+}
