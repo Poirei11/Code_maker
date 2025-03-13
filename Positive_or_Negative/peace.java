1a2b55f95b87c70055ea3470713992502eb5c2b8
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 11:16:14 +0530

    Peace

---
+++ Positive_or_Negative/peace.java
@@ -0,0 +1,11 @@
+public class peace{
+   public static void main(String args[]){
+    int a = 20;
+    if(a>15){
+        System.out.println("positive");
+    }
+    else{
+         System.out.println("Negative");                
+    }
+   } 
+}
