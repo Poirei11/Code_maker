2f08082b92cd7a6f2633022900aa97a4dcb98bce
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 11:55:39 +0530

    Peace

---
+++ else_if_statement/peace.java
@@ -0,0 +1,17 @@
+public class peace{
+    public static void main(String args[]){
+        int a = 45;
+        if(a>0){
+            System.out.println("Positive");
+
+        }
+        else if(a<0){
+            System.out.println("Negative");
+
+        }
+        else{
+            System.out.println("The no. is zero");
+
+        }
+    }
+}
