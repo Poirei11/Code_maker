1569ce4518776f735b7a99b08810c4f8067e2495
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 11:13:58 +0530

    Peace

---
+++ Major_or_Minor_Finder/peace.java
@@ -0,0 +1,11 @@
+public class peace{
+   public static void main(String args[]){
+    int a = 20;
+    if(a>15){
+        System.out.println("minor");
+    }
+    else{
+         System.out.println("major");                
+    }
+   } 
+}
