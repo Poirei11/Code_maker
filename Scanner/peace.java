8f15305824736d2ed162b4ee787e35a37f200c93
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 14:49:53 +0530

    peace

---
+++ Scanner/peace.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class peace{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = input.nextLine();
+        System.out.println(name);
+    }
+}
