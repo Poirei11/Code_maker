8a96a510cc8e26ea3f01d0fc35d0acc8a387996d
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 10:33:49 +0530

    Conditional

---
+++ Conditional_operator/condi.java
@@ -0,0 +1,10 @@
+public class condi{
+    public static void main(String args[]){
+        System.out.println(10<2);
+        System.out.println(12>34);
+        System.out.println(40>=50);
+        System.out.println(43<=45);
+        System.out.println(40==50);
+        System.out.println(40!=50);
+    }
+}
