78fcebaad0bac1f658e296d011696d22df386c4a
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 15:11:06 +0530

    peace

---
+++ Next_int/peace.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class peace{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Age");
+        int age = input.nextInt();
+        System.out.println("Your age is " + age);
+    }
+}
