40ffd4e1ac6e36d809c30b7c1979f64c39f88ff2
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 10:55:53 +0530

    Condition

---
+++ Conditional_statement/if_statement/peace.java
@@ -0,0 +1,10 @@
+public class peace{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("I know u");
+        }
+        if(false){
+            System.out.println("I don't know u");
+        }
+    }
+}
