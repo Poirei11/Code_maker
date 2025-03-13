bfb951d83729c573e90e7d0bdb070bce07285f3a
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 15:17:40 +0530

    peace

---
+++ Dynamic_addition/peace.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class peace{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("First no.");
+        int n = input.nextInt();
+        System.out.println("Second no.");
+        int m = input.nextInt();
+        int a = n+m;
+        System.out.println("The sum is" + a);
+    }
+}
