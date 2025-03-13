bbf610da067311a9bbfab33a1d320d25d4fead50
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 11:42:45 +0530

    Peace

---
+++ Gender_finder/peace.java
@@ -0,0 +1,11 @@
+public class peace{
+   public static void main(String args[]){
+    String a = "M";
+    if(a == "M"){
+        System.out.println("Male");
+    }
+    else{
+         System.out.println("Female");                
+    }
+   } 
+}
