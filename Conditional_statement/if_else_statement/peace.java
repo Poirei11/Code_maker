d7f076f9ec3fde83afbddc98722523ba67e8e965
Poirei11 <thokchompoirei4@gmail.com>
2025-03-13 11:03:30 +0530

    Peace

---
+++ Conditional_statement/if_else_statement/peace.java
@@ -0,0 +1,11 @@
+public class peace{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("Samir is gay");
+        }
+        else{
+            System.out.println("Samir is gay");
+        }
+    }
+}
+    
