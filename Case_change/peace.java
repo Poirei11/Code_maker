f1dd688ddcf83f7761a3597ecf79efbc9cbe0204
Poirei11 <thokchompoirei4@gmail.com>
2025-03-14 12:00:09 +0530

    peace

---
+++ Case_change/peace.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class peace{
+    public static void main(String args[]){
+        String name = "hello,world";
+       
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
