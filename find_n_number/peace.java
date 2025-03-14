190cdf4db597ca52879246b352d62be7da524f11
Poirei11 <thokchompoirei4@gmail.com>
2025-03-14 10:18:04 +0530

    peace

---
+++ find_n_number/peace.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+public class peace{
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the nummber");
+        int a = obj.nextInt();
+        int i = 1;
+        while(i<=a){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
