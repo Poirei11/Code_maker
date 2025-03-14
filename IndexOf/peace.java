167906f30edf1384aa60693bb186c35f4d1f9abc
Poirei11 <thokchompoirei4@gmail.com>
2025-03-14 12:18:53 +0530

    peace

---
+++ IndexOf/peace.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class peace{
+    public static void main(String args[]){
+        String name = "hello,world";
+        
+        System.out.println(name.indexOf("w"));
+    }
+}
