32320088f10f322a02e96b98634c786ff0484699
Poirei11 <thokchompoirei4@gmail.com>
2025-03-14 10:02:41 +0530

    peace

---
+++ While_loop/peace.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class peace{
+    public static void main(String args[]){
+        int i = 92;
+        while(i<98){
+        System.out.println(i);
+        i++;
+        }
+    }
+}
